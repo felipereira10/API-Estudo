5725bf1feedd46c1466ca60ca821922e1d102267
Felipe Pereira <126419915+felipereira10@users.noreply.github.com>
2024-11-21 17:22:59 -0300

    feat: CRUD finalizado

--- server.js
+++ server.js
@@ -8,20 +8,71 @@ app.use(express.json())
 
 const users = []
 
-app.post('/usuarios', (req, res) => {
+app.post('/usuarios', async (req, res) => {
 
-    users.push(req.body)
+    await prisma.user.create({
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    });
 
     res.status(201).json(req.body)
 });
 
-app.get('/usuarios', (req, res) => {
+app.get('/usuarios', async (req, res) => {
+
+    let users = []
+
+    if (req.query) {
+        users = await prisma.user.findMany({
+            where: {
+                name: req.query.name,
+                age: req.query.age,
+                email: req.query.email
+            }
+        }) 
+
+    }else{
+        users = await prisma.user.findMany()
+    }
+
+
     res.status(200).json(users)
 });
 
+//:id -> os ":" identifica que é uma variável
+app.put('/usuarios/:id', async (req, res) => {
+
+    await prisma.user.update({
+        where: {
+            id: req.params.id
+        },
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    })
+
+    res.status(201).json(req.body)
+ });
+
+ app.delete('/usuarios/:id', async (req, res) => {
+
+    await prisma.user.delete({
+        where: {
+            id: req.params.id
+        },
+    })
+
+    res.status(201).json({message:'Usuário deletado com sucesso!'})
+ });
+
 app.listen(3000)
 
-/*
+/*a
     Criar nossa API de Usuários:
     - Criar um usuário
     - Listar todos os usuários
